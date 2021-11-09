6177619df23eb56aef0a46f95151ad63ba6ed093
seilylook <seilylook@naver.com>
2021-11-09 14:08:29 +0900

    feat: index router 설정해라

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 const Koa = require('Koa');
+const Router = require('koa-router');
 
 const app = new Koa();
+const router = new Router();
 
-app.use((ctx) => {
-  ctx.body = 'hello world';
+router.get('/', (ctx) => {
+  ctx.body = '홈';
 });
 
+router.get('/about', (ctx) => {
+  ctx.body = '소개';
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
 app.listen(4000, () => {
-  console.log('Listening to port 4000');
+  console.log('listending to port 4000');
 });
